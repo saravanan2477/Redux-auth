690cc08a53782c6796383db00a816fec8ab55d66
saravanan <saravanan.k1357@gmail.com>
2024-11-19 14:03:22 +0530

    created header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
+import Header from './components/Header';
 
 
 
@@ -10,7 +11,8 @@ export default function App() {
   return (
     <BrowserRouter>
    
-   
+   {/* header */}
+   <Header />
       <Routes>
        <Route path='/' element = {<Home/>} />
        <Route path='/profile' element = {<Profile/>} />
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div>
- Home1111dxd
+      Home
     </div>
   )
 }
